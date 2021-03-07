7586be2f82afa1df16a2abc317849b24e9a6921d
André Felipe Matos de Andrade <andrefmandrade@outlook.com>
2021-03-07 12:48:01 -0300

    Removido ResetPasswordError (depreciado)

--- api/src/infra/middlewares/errors/processingError.js
+++ api/src/infra/middlewares/errors/processingError.js
@@ -1,16 +1,11 @@
 const path = require('path');
 const AppError = require('../../../shared/errors/AppError');
-const ResetPasswordError = require('../../../shared/errors/ResetPasswordError');
 
 function errorHandler(error, req, res, next) {
   if (error instanceof AppError) return res.json({
     success: false,
     message: error.message
   });
-
-  if (error instanceof ResetPasswordError) return res.sendFile(
-    path.resolve('public', 'error', 'index.html')
-  );
 
   console.log(error);
   return res.json({
