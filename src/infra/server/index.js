351b7e24ad5d56ce15ddd3aa894820cacb3a3651
Jefferson Patricio <jeffersonpatricio1@gmail.com>
2021-03-17 19:22:08 -0300

    add cors

--- src/infra/server/index.js
+++ src/infra/server/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 require('express-async-errors');
 const helmet = require('helmet');
+const cors = require('cors');
 const routes = require('../routes');
 
 const app = express();
 
 app.use(helmet());
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
