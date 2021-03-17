e5c384d29230e3fc0641c0c84d1aac005d98e816
Jefferson Patricio <jeffersonpatricio1@gmail.com>
2021-03-17 10:47:26 -0300

    atualizacao deploy heroku

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,12 @@
 const { serverPort } = require('./shared/config/server');
 const server = require('./infra/server');
 
+const https = require('https');
+
+setInterval(() => {
+  https.get('https://joindev-api.herokuapp.com/ping');
+}, 1200000);
+
 server.listen(serverPort, () => {
   console.log('Server running in port ' + serverPort);
 });
