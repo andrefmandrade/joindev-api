767538ae6b8b6aff853af890982bee2e6abd7e7e
Jefferson Patricio <jeffersonpatricio1@gmail.com>
2021-03-17 21:25:40 -0300

    route check auth and change email reset password link

--- src/infra/routes/index.js
+++ src/infra/routes/index.js
@@ -11,13 +11,7 @@ routes.get('/favicon.ico', (_, res) =>
   res.sendFile(path.resolve('public', 'favicon.ico'))
 );
 
-routes.get('/ping', (_, res) =>
-  res.send(`Todos as descrições das pessoas são sobre a humanidade do atendimento, a pessoa pega no pulso, examina, olha com carinho. Então eu acho que vai ter outra coisa, que os médicos cubanos trouxeram pro brasil, um alto grau de humanidade.
-
-A única área que eu acho, que vai exigir muita atenção nossa, e aí eu já aventei a hipótese de até criar um ministério. É na área de... Na área... Eu diria assim, como uma espécie de analogia com o que acontece na área agrícola.
-
-Eu dou dinheiro pra minha filha. Eu dou dinheiro pra ela viajar, então é... é... Já vivi muito sem dinheiro, já vivi muito com dinheiro. -Jornalista: Coloca esse dinheiro na poupança que a senhora ganha R$10 mil por mês. -Dilma: O que que é R$10 mil?`)
-);
+routes.get('/version', (_, res) => res.send('1.0.0'));
 
 routes.use('/users', usersRoutes);
 routes.use('/sessions', sessionsRoutes);
