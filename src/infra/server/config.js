87503c2fe35845e83f1902d685cb315194f742c1
Jefferson Patricio <jeffersonpatricio1@gmail.com>
2021-03-24 19:51:47 -0300

    cors update

--- src/infra/server/config.js
+++
@@ -1,1 +0,0 @@
-//TODO: trazer a função src/infra/server/index.js -> corsOptionsDelegate() para esse arquivo
--- src/infra/server/index.js
+++ src/infra/server/index.js
@@ -6,28 +6,9 @@ const routes = require('../routes');
 
 const app = express();
 
-const allowList = [
-  'https://joindev-reactjs.vercel.app/',
-  'http://localhost:3000/',
-];
-
-const corsOptionsDelegate = (req, callback) => {
-  let corsOptions;
-
-  let isDomainAllowed = allowList.indexOf(req.header('Origin')) !== -1;
-
-  if (isDomainAllowed) {
-    // Enable CORS for this request
-    corsOptions = { origin: true };
-  } else {
-    // Disable CORS for this request
-    corsOptions = { origin: false };
-  }
-  callback(null, corsOptions);
-};
-
 app.use(cors());
 app.use(helmet());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
