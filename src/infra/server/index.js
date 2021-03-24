9831503a9dfffd1e9921d472e2facae6b514ebb8
Jefferson Patricio <jeffersonpatricio1@gmail.com>
2021-03-24 19:58:19 -0300

    cors update

--- src/infra/server/index.js
+++ src/infra/server/index.js
@@ -6,10 +6,10 @@ const routes = require('../routes');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(helmet());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 app.use(routes);
 
 module.exports = app;
