83c397e5a17eae000c8ff69140b5dcef9bd1e4e5
André Felipe Matos de Andrade <andrefmandrade@outlook.com>
2021-04-30 17:22:59 -0300

    remove setInterval

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 const { serverPort } = require('./shared/config/server');
 const server = require('./infra/server');
 
-const https = require('https');
-
-setInterval(() => {
-  https.get('https://joindev-api.herokuapp.com/ping');
-}, 1200000);
-
 server.listen(serverPort, () => {
   console.log('Server running in port ' + serverPort);
 });
